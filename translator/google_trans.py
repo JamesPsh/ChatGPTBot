f21ed1db9b23b0223761820a8dca1f1e45c91251
sanhapark <jamesanha.@kakao.com>
2023-06-13 08:11:39 +0900

    Add translation feature using googletrans library

---
+++ translator/google_trans.py
@@ -0,0 +1,28 @@
+import logging
+from googletrans import Translator, LANGUAGES
+
+
+'''
+https://pypi.org/project/googletrans/
+'''
+
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+
+def translate(text, dest='en'):
+
+    # Check if the language codes are valid
+    if dest not in LANGUAGES.keys():
+        logging.error(f"Unsupported destination language code: {dest}")
+        return "Error: Unsupported destination language code."
+
+    translator = Translator()
+    
+    try:
+        ans = translator.translate(text, dest=dest)
+        logging.info(f"Translation successful. Destination: {dest}")
+        return ans.text
+    except Exception as e:
+        logging.error(f"Translation failed with error: {e}")
+        return f"Error: Translation failed. {str(e)}"
