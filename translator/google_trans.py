3e77a168f35b88cd66fbe4e7d7ec93d23189e49e
sanhapark <jamesanha.@kakao.com>
2023-06-13 08:13:32 +0900

    Add example usage to test translation functionality

--- translator/google_trans.py
+++ translator/google_trans.py
@@ -26,3 +26,12 @@ def translate(text, dest='en'):
     except Exception as e:
         logging.error(f"Translation failed with error: {e}")
         return f"Error: Translation failed. {str(e)}"
+
+
+if __name__ == '__main__':
+    text = '안녕하세요'
+    target_language = 'en'
+    
+    translated_text = translate(text, target_language)
+    print(f"Original Text: {text}")
+    print(f"Translated Text: {translated_text}")
